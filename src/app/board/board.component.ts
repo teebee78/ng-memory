64b23766c504c36c58c42447cc7adec92dd0bd3b
tom <tombuehler@ymail.com>
2024-08-15 20:09:22 +0200

    styling and scores

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import { CardComponent } from '../card/card.component';
+import { createFind } from 'rxjs/internal/operators/find';
 
 @Component({
   selector: 'app-board',
@@ -16,59 +17,33 @@ import { CardComponent } from '../card/card.component';
 })
 export class BoardComponent {
   cards = input.required<string[]>();
-  flipHistory = signal<number[]>([]);
   
   flips: Array<{index: number, symbol: string}> = [];
-
+  discoveredSymbols: string[] = [];
+  flippedIndices: number[] = [];
+  attempts = 0;
   penalty = 0;
-
-
-  discovered = computed(() => {
-    const flipedSymbols = this.flipHistory()
-      .map((each) => this.cards().at(each))
-      .sort();
-
-    const discoveredSymbols: string[] = [];
-    for (
-      let index = 0;
-      index < this.flipHistory().length - 1 - (this.flipHistory().length & 1);
-      index++
-    ) {
-      if (
-        this.flipHistory().length > 1 &&
-        this.flipHistory()[index] !== this.flipHistory()[index + 1] &&
-        this.cards()[this.flipHistory()[index]] ===
-          this.cards()[this.flipHistory()[index + 1]]
-      ) {
-        discoveredSymbols.push(this.cards()[this.flipHistory()[index]]!);
-      }
-    }
-    return discoveredSymbols;
-  });
-
-  flippedIndices = computed(() => {
-    return this.flipHistory().length & 1
-      ? this.flipHistory().slice(-1)
-      : this.flipHistory().slice(-2);
-  });
   
-  numberOfAttempts = computed(() => this.flipHistory().length);
-
   onFlip(cardIndex: number): void {
-    const [last] = this.flipHistory().slice(-1);
-    if (last === cardIndex) {
+    const [last] = this.flips.slice(-1);
+    if (this.flippedIndices.includes(cardIndex)) { // the clicked card is already flipped. 
+      this.flippedIndices = this.flips.length % 2 == 0 ? [] : this.flippedIndices;
       return;
     }
-    this.flipHistory.update((current) => current.concat(cardIndex));
 
-
-    // new way without internal signals
     this.flips.push({index: cardIndex, symbol: this.cards().at(cardIndex)!});
     if (this.flips.length % 2 == 0) {
-      var [first, second] = this.flips.slice(-2);
-      if (first.symbol !== second.symbol) {
-        this.penalty += this.flips.filter(({symbol}) => symbol === second.symbol).length - 1;;
+      this.attempts++;
+      var [secondLastFlip, lastFlip] = this.flips.slice(-2);
+      this.flippedIndices = [secondLastFlip.index, lastFlip.index];
+      if (secondLastFlip.symbol !== lastFlip.symbol) {
+        this.penalty += this.flips.filter(({symbol}) => symbol === lastFlip.symbol).length - 1;;
+      } else {
+        this.discoveredSymbols.push(secondLastFlip.symbol);
       }
+    } else {
+      var [lastFlip] = this.flips.slice(-1);
+      this.flippedIndices = lastFlip ? [lastFlip.index] : [];
     }
   }
 }
